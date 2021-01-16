6c34aa05394da3fa5c2d62d6a273c360bcbb6c45
SaschaDoe <sascha.doerflein@web.de>
2021-01-16 17:32:48 +0100

    Added GameOfLifeOOP.csproj Added GameOfLifeOOPTests.csproj

---
+++ Katas/GameOfLifeOOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameOfLifeOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
