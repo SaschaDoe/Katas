69964a323812575f8dd69c9e1bc02fa04a8d9f46
Sascha Dörflein <sascha.doerflein@infoteam.de>
2021-09-24 09:58:35 +0200

    Added documentation Added CsvHeaderTests.cs Added CsvColumnTests.cs

---
+++ 1. Function Katas/1/CSVTable/CSVTableTests/UnitTests/CsvColumnTests.cs
@@ -0,0 +1,82 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using CSVTable.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSVTableTests.UnitTests
+{
+    [TestClass]
+    public class CsvColumnTests
+    {
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void ofEmptyStringList()
+        {
+
+            var actualCsvColumn = CsvColumn.Of(new List<string>());
+            var expectedEnumerable = System.Array.Empty<object>();
+            var actualEnumerable = actualCsvColumn.GetColumns();
+            Assert.IsTrue(expectedEnumerable.SequenceEqual(actualEnumerable));
+        }
+        
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void ofOnlyOneHeader()
+        {
+
+            var actualCsvColumn = CsvColumn.Of(new List<string>(){"Name"});
+            var expectedEnumerable = new List<string>() {"Name|", "----+"};
+            var actualEnumerable = actualCsvColumn.GetColumns();
+            Assert.IsTrue(expectedEnumerable.SequenceEqual(actualEnumerable));
+        }
+        
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void ofHeaderAndOneLine()
+        {
+
+            var actualCsvColumn = CsvColumn.Of(new List<string>(){"Name","Bla"});
+            var expectedEnumerable = new List<string>() {"Name|", "----+", "Bla |"};
+            var actualEnumerable = actualCsvColumn.GetColumns();
+            Assert.IsTrue(expectedEnumerable.SequenceEqual(actualEnumerable));
+        }
+        
+
+        
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void ofHeaderAndOneLine_And_DataBiggerThanHeader()
+        {
+
+            var actualCsvColumn = CsvColumn.Of(new List<string>(){"Name","BlaBla"});
+            var expectedEnumerable = new List<string>() {"Name  |", "------+", "BlaBla|"};
+            var actualEnumerable = actualCsvColumn.GetColumns();
+            Assert.IsTrue(expectedEnumerable.SequenceEqual(actualEnumerable));
+        }
+        
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void ofHeaderAndMoreLines_And_DataBiggerThanHeader()
+        {
+
+            var actualCsvColumn = CsvColumn.Of(new List<string>(){"Name","BlaBla", "Bla"});
+            var expectedEnumerable = new List<string>() {"Name  |", "------+", "BlaBla|", "Bla   |"};
+            var actualEnumerable = actualCsvColumn.GetColumns();
+            Assert.IsTrue(expectedEnumerable.SequenceEqual(actualEnumerable));
+        }
+        
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void ofAgeList()
+        {
+
+            var actualCsvColumn = CsvColumn.Of(new List<string>(){"Alter","42", "43", "65"});
+            var expectedEnumerable = new List<string>() {"Alter|", "-----+", "42   |", "43   |", "65   |"};
+            var actualEnumerable = actualCsvColumn.GetColumns();
+            Assert.IsTrue(expectedEnumerable.SequenceEqual(actualEnumerable));
+        }
+        
+    }
+}
---
+++ 1. Function Katas/1/CSVTable/CSVTableTests/UnitTests/CsvHeaderTests.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CSVTable.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSVTableTests.UnitTests
+{
+    [TestClass]
+    public class CsvHeaderTests
+    {
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void ofNothing()
+        {
+            var actualCsvColumn = CsvHeader.Of(string.Empty);
+            var expectedEnumerable = System.Array.Empty<object>();
+            Assert.IsTrue(expectedEnumerable.SequenceEqual(actualCsvColumn.GetColumns()));
+        }
+        
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void ofOnlyOneHeader()
+        {
+            var actualCsvColumn = CsvHeader.Of("Name");
+            var expectedEnumerable = new List<string>() {"Name", "----"};
+            Assert.IsTrue(expectedEnumerable.SequenceEqual(actualCsvColumn.GetColumns()));
+        }
+    }
+}
